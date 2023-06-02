f4889d9526089820cd125f8ba055a16302e6106e
Omonge Calvince <calmosh1421@gmail.com>
2023-06-02 10:39:27 +0300

    cache control and authorization header

--- src/main/java/dev/vexla/mpesaDaraja/shared/Constants.java
+++ src/main/java/dev/vexla/mpesaDaraja/shared/Constants.java
@@ -1,5 +1,8 @@
 package dev.vexla.mpesaDaraja.shared;
 
 public class Constants {
-
+    public static final String BASIC_AUTH_STRING = "Basic";
+    public static final String AUTHORIZATION_HEADER_STRING = "authorization";
+    public static final String CACHE_CONTROL_HEADER = "cache-control";
+    public static final String CACHE_CONTROL_HEADER_VALUE = "no-control";
 }
