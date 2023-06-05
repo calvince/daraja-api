203080f927f50fb53b654ca6cf94c5bea0289aa9
Omonge Calvince <calmosh1421@gmail.com>
2023-06-05 05:26:23 +0300

    a bean to inject object across application level

--- src/main/java/dev/vexla/mpesaDaraja/config/OkhttpConfig.java
+++ src/main/java/dev/vexla/mpesaDaraja/config/OkhttpConfig.java
@@ -1,7 +1,14 @@
 package dev.vexla.mpesaDaraja.config;
 
+import okhttp3.OkHttpClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class OkhttpConfig {
+
+    @Bean
+    public OkHttpClient getOkHttpClient() {
+        return new OkHttpClient();
+    }
 }
