c9bf936ba5f12bcd4e20adb036bd78bc81547454
Omonge Calvince <calmosh1421@gmail.com>
2023-06-05 17:24:03 +0300

    toString method and accessToken property

--- src/main/java/dev/vexla/mpesaDaraja/dto/AccessToken.java
+++ src/main/java/dev/vexla/mpesaDaraja/dto/AccessToken.java
@@ -1,4 +1,22 @@
 package dev.vexla.mpesaDaraja.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.Data;
+import lombok.SneakyThrows;
+
+@Data
 public class AccessToken {
+
+    @JsonProperty("access_token")
+    private String accessToken;
+
+    @JsonProperty("expires_in")
+    private String expiresIn;
+
+    @SneakyThrows
+    @Override
+    public String toString() {
+        return new ObjectMapper().writeValueAsString(this);
+    }
 }
