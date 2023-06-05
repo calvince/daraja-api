9d8e2c3055f558cb174ce880e1f140a9eed0f2fb
Omonge Calvince <calmosh1421@gmail.com>
2023-06-05 05:17:20 +0300

    a bean to inject object across application level

--- src/main/java/dev/vexla/mpesaDaraja/config/MapperConfig.java
+++ src/main/java/dev/vexla/mpesaDaraja/config/MapperConfig.java
@@ -1,4 +1,14 @@
 package dev.vexla.mpesaDaraja.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class MapperConfig {
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        return new ObjectMapper();
+    }
 }
