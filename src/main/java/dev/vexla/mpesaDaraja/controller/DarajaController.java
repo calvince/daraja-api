6e4667b6553ad596fc5d6ca01ecd794df9e833bc
Omonge Calvince <calmosh1421@gmail.com>
2023-06-05 06:41:23 +0300

    object injection using constructor

--- src/main/java/dev/vexla/mpesaDaraja/controller/DarajaController.java
+++ src/main/java/dev/vexla/mpesaDaraja/controller/DarajaController.java
@@ -1,10 +1,20 @@
 package dev.vexla.mpesaDaraja.controller;
 
+import dev.vexla.mpesaDaraja.service.DarajaApi;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/mpesa")
 public class DarajaController {
+
+    private final DarajaApi darajaApi;
+
+    public DarajaController(DarajaApi darajaApi) {
+        this.darajaApi = darajaApi;
+    }
+
 
 }
