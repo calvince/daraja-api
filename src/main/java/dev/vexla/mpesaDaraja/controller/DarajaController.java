3ad41eda9b35b012700478010ec8afdd4fc7769b
Omonge Calvince <calmosh1421@gmail.com>
2023-06-05 05:58:20 +0300

    initial commit

---
+++ src/main/java/dev/vexla/mpesaDaraja/controller/DarajaController.java
@@ -0,0 +1,10 @@
+package dev.vexla.mpesaDaraja.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/mpesa")
+public class DarajaController {
+
+}
