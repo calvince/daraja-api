e34ed96bed95e12de0399a37253433f69dce4da8
Omonge Calvince <calmosh1421@gmail.com>
2023-06-06 21:40:02 +0300

    use Jackson to Decode the ResponseBody

--- src/main/java/dev/vexla/mpesaDaraja/service/DarajaApiImpl.java
+++ src/main/java/dev/vexla/mpesaDaraja/service/DarajaApiImpl.java
@@ -3,10 +3,17 @@ package dev.vexla.mpesaDaraja.service;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import dev.vexla.mpesaDaraja.config.DarajaConfiguration;
 import dev.vexla.mpesaDaraja.dto.AccessToken;
+import dev.vexla.mpesaDaraja.shared.Constants;
 import dev.vexla.mpesaDaraja.shared.Helper;
 import lombok.extern.slf4j.Slf4j;
 import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+
+import static dev.vexla.mpesaDaraja.shared.Constants.*;
 
 @Service
 @Slf4j
@@ -25,8 +32,24 @@ public class DarajaApiImpl implements DarajaApi {
     public AccessToken getAccessToken() {
         log.info("*** Returns Daraja Api access token response; get token *");
         // get the Base64 of consumerKey + ":" + consumerSecret
-        String encodedCredentials = Helper.toBase64(String.format("%s:%s", darajaConfiguration.getConsumerKey(), darajaConfiguration.getConsumerSecret()));
+        String encodedCredentials = Helper.toBase64(String.format("%s:%s",
+                darajaConfiguration.getConsumerKey(), darajaConfiguration.getConsumerSecret()));
+        Request request = new Request.Builder()
+                .url(String.format("%s?grant_type=%s", darajaConfiguration.getOauthEndpoint(), darajaConfiguration.getGrantType()))
+                .get()
+                .addHeader(AUTHORIZATION_HEADER_STRING, String.format("%s %s", BASIC_AUTH_STRING, encodedCredentials))
+                .addHeader(CACHE_CONTROL_HEADER, CACHE_CONTROL_HEADER_VALUE)
+                .build();
 
-        return null;
+        try {
+            Response response = okHttpClient.newCall(request).execute();
+            assert response.body() != null;
+
+            //use Jackson to Decode the ResponseBody
+            return objectMapper.readValue(response.body().string(), AccessToken.class);
+        } catch (IOException e) {
+            log.error(String.format("Could not get access token. -> %s", e.getLocalizedMessage()));
+            return null;
+        }
     }
 }
