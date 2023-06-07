dd0371554b9a178604e4f351de545a4edbab71c5
Omonge Calvince <calmosh1421@gmail.com>
2023-06-07 16:40:49 +0300

    take object and returns json string

--- src/main/java/dev/vexla/mpesaDaraja/shared/Helper.java
+++ src/main/java/dev/vexla/mpesaDaraja/shared/Helper.java
@@ -1,9 +1,12 @@
 package dev.vexla.mpesaDaraja.shared;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 
 import java.nio.charset.StandardCharsets;
 import java.util.Base64;
+import java.util.Objects;
 
 @Slf4j
 public class Helper {
@@ -12,5 +15,13 @@ public class Helper {
     public static String toBase64(String value) {
         byte[] data = value.getBytes(StandardCharsets.ISO_8859_1);
         return Base64.getEncoder().encodeToString(data);
+    }
+
+    public static String toJson(Object object) {
+        try {
+            return new ObjectMapper().writeValueAsString(object);
+        } catch (JsonProcessingException e) {
+            return null;
+        }
     }
 }
