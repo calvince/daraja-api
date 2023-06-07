c895ed5c9f319f748383a492fdb5320d8901afc1
Omonge Calvince <calmosh1421@gmail.com>
2023-06-07 12:29:12 +0300

    registerUrl method without body

--- src/main/java/dev/vexla/mpesaDaraja/service/DarajaApi.java
+++ src/main/java/dev/vexla/mpesaDaraja/service/DarajaApi.java
@@ -1,7 +1,9 @@
 package dev.vexla.mpesaDaraja.service;
 
-import dev.vexla.mpesaDaraja.dto.AccessToken;
+import dev.vexla.mpesaDaraja.dto.response.AccessToken;
+import dev.vexla.mpesaDaraja.dto.response.RegisterUrlResponse;
 
 public interface DarajaApi {
     AccessToken getAccessToken();
+    RegisterUrlResponse registerUrl();
 }
