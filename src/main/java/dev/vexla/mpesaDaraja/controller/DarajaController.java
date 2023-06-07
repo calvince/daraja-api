225f049d415725060c196dab87dab070ecdf151c
Omonge Calvince <calmosh1421@gmail.com>
2023-06-07 12:31:05 +0300

    api to register url

--- src/main/java/dev/vexla/mpesaDaraja/controller/DarajaController.java
+++ src/main/java/dev/vexla/mpesaDaraja/controller/DarajaController.java
@@ -1,9 +1,11 @@
 package dev.vexla.mpesaDaraja.controller;
 
-import dev.vexla.mpesaDaraja.dto.AccessToken;
+import dev.vexla.mpesaDaraja.dto.response.AccessToken;
+import dev.vexla.mpesaDaraja.dto.response.RegisterUrlResponse;
 import dev.vexla.mpesaDaraja.service.DarajaApi;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,5 +22,11 @@ public class DarajaController {
     @GetMapping(path = "/token", produces = "application/json")
     public ResponseEntity<AccessToken> getAccessToken() {
         return ResponseEntity.ok(this.darajaApi.getAccessToken());
+    }
+
+    //register uri
+    @PostMapping(path = "/register-url", produces = "application/json")
+    public ResponseEntity<RegisterUrlResponse> registerUrl() {
+        return ResponseEntity.ok(this.darajaApi.registerUrl());
     }
 }
