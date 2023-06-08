cfa7106c167ff071ac9da34985903d8b92332810
Omonge Calvince <calmosh1421@gmail.com>
2023-06-08 13:00:30 +0300

    json mediaType constant

--- src/main/java/dev/vexla/mpesaDaraja/shared/Constants.java
+++ src/main/java/dev/vexla/mpesaDaraja/shared/Constants.java
@@ -1,8 +1,12 @@
 package dev.vexla.mpesaDaraja.shared;
 
+import okhttp3.MediaType;
+
 public class Constants {
     public static final String BASIC_AUTH_STRING = "Basic";
     public static final String AUTHORIZATION_HEADER_STRING = "authorization";
     public static final String CACHE_CONTROL_HEADER = "cache-control";
     public static final String CACHE_CONTROL_HEADER_VALUE = "no-control";
+
+    public static MediaType JSON_MEDIA_TYPE = MediaType.parse("application/json; charset=UTF-8");
 }
