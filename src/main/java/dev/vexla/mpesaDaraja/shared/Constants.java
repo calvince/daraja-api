dd498946caa611e905db2fce3cdda7d50c3e7ab7
Omonge Calvince <calmosh1421@gmail.com>
2023-06-11 00:19:33 +0300

    code refactor

--- src/main/java/dev/vexla/mpesaDaraja/shared/Constants.java
+++ src/main/java/dev/vexla/mpesaDaraja/shared/Constants.java
@@ -7,6 +7,5 @@ public class Constants {
     public static final String AUTHORIZATION_HEADER_STRING = "authorization";
     public static final String CACHE_CONTROL_HEADER = "cache-control";
     public static final String CACHE_CONTROL_HEADER_VALUE = "no-control";
-
-    public static MediaType JSON_MEDIA_TYPE = MediaType.parse("application/json; charset=UTF-8");
+    public static final MediaType JSON_MEDIA_TYPE = MediaType.parse("application/json; charset=utf-8");
 }
