b5cd03941aa80ca6bb184eb1a9b5863145890f2c
Omonge Calvince <calmosh1421@gmail.com>
2023-06-11 00:15:01 +0300

    change api url

--- src/main/java/dev/vexla/mpesaDaraja/controller/DarajaController.java
+++ src/main/java/dev/vexla/mpesaDaraja/controller/DarajaController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/mpesa")
+@RequestMapping("/mobile-money")
 public class DarajaController {
 
     private final DarajaApi darajaApi;
