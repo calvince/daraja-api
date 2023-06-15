0fa4782394a895f9d1089cc1fb082447a1576cd1
calvince <calmosh1421@gmail.com>
2023-06-16 01:59:13 +0300

    payload request in simulating c2b transaction

---
+++ src/main/java/dev/vexla/mpesaDaraja/dto/SimulateC2BRequest.java
@@ -0,0 +1,23 @@
+package dev.vexla.mpesaDaraja.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+
+@Data
+public class SimulateC2BRequest{
+
+	@JsonProperty("ShortCode")
+	private String shortCode;
+
+	@JsonProperty("Msisdn")
+	private String msisdn;
+
+	@JsonProperty("BillRefNumber")
+	private String billRefNumber;
+
+	@JsonProperty("Amount")
+	private String amount;
+
+	@JsonProperty("CommandID")
+	private String commandID;
+}
