9e48d50c24ad97f73fde78a6be939ecf5ee01f3a
calvince <calmosh1421@gmail.com>
2023-06-16 01:56:46 +0300

    create bearer Constants

--- src/main/java/dev/vexla/mpesaDaraja/shared/Constants.java
+++ src/main/java/dev/vexla/mpesaDaraja/shared/Constants.java
@@ -4,7 +4,8 @@ import okhttp3.MediaType;
 
 public class Constants {
     public static final String BASIC_AUTH_STRING = "Basic";
-    public static final String AUTHORIZATION_HEADER_STRING = "authorization";
+    public static final String AUTHORIZATION_HEADER_STRING = "Authorization";
+    public static final String BEARER_AUTH_STRING = "Bearer";
     public static final String CACHE_CONTROL_HEADER = "cache-control";
     public static final String CACHE_CONTROL_HEADER_VALUE = "no-control";
     public static final MediaType JSON_MEDIA_TYPE = MediaType.parse("application/json; charset=utf-8");
