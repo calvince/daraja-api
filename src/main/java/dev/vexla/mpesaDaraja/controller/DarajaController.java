974a034626e62e62ad434c201deaf6beeaae38d7
calvince <calmosh1421@gmail.com>
2023-07-02 10:37:18 +0300

    B2C transaction api

--- src/main/java/dev/vexla/mpesaDaraja/controller/DarajaController.java
+++ src/main/java/dev/vexla/mpesaDaraja/controller/DarajaController.java
@@ -1,25 +1,29 @@
 package dev.vexla.mpesaDaraja.controller;
 
-import dev.vexla.mpesaDaraja.dto.SimulateC2BRequest;
-import dev.vexla.mpesaDaraja.dto.SimulateC2BResponse;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import dev.vexla.mpesaDaraja.dto.request.InternalB2CTransactionRequest;
+import dev.vexla.mpesaDaraja.dto.request.SimulateC2BRequest;
+import dev.vexla.mpesaDaraja.dto.response.*;
 import dev.vexla.mpesaDaraja.dto.request.TransactionResult;
-import dev.vexla.mpesaDaraja.dto.response.AccessToken;
-import dev.vexla.mpesaDaraja.dto.response.AcknowledgeResponse;
-import dev.vexla.mpesaDaraja.dto.response.RegisterUrlResponse;
 import dev.vexla.mpesaDaraja.service.DarajaApi;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/mobile-money")
+@Slf4j
 public class DarajaController {
 
     private final DarajaApi darajaApi;
     private final AcknowledgeResponse acknowledgeResponse;
+    private final ObjectMapper objectMapper;
 
-    public DarajaController(DarajaApi darajaApi, AcknowledgeResponse acknowledgeResponse) {
+    public DarajaController(DarajaApi darajaApi, AcknowledgeResponse acknowledgeResponse, ObjectMapper objectMapper) {
         this.darajaApi = darajaApi;
         this.acknowledgeResponse = acknowledgeResponse;
+        this.objectMapper = objectMapper;
     }
 
     @GetMapping(path = "/token", produces = "application/json")
@@ -43,5 +47,25 @@ public class DarajaController {
     @PostMapping(path = "/simulate-c2b", produces = "application/json")
     public ResponseEntity<SimulateC2BResponse> simulateC2BTransaction(@RequestBody SimulateC2BRequest simulateC2BRequest) {
         return  ResponseEntity.ok(this.darajaApi.simulateC2BTransaction(simulateC2BRequest));
+    }
+
+    //B2C Transaction Region
+
+    //expose callback url endpoint
+    @PostMapping(path = "/b2c-transaction-result", produces = "application/json")
+    public ResponseEntity<AcknowledgeResponse> execute(@RequestBody B2CTransactionAsyncResponse response) throws JsonProcessingException {
+        log.info("==================B2C Transaction Response =================");
+        log.info(objectMapper.writeValueAsString(response));
+        return ResponseEntity.ok(acknowledgeResponse);
+    }
+
+    @PostMapping(path = "/b2c-queue-timeout", produces = "application/json")
+    public ResponseEntity<AcknowledgeResponse> queueTimeout(@RequestBody Object object)  {
+        return ResponseEntity.ok(acknowledgeResponse);
+    }
+
+    @PostMapping(path = "/b2c-transaction", produces = "application/json")
+    public ResponseEntity<B2CTransactionSyncResponse> performB2CTransaction(@RequestBody InternalB2CTransactionRequest b2CTransactionRequest) {
+        return ResponseEntity.ok(this.darajaApi.performB2CTransaction(b2CTransactionRequest));
     }
 }
