ad8d46f313e63e5a765efd46733f9906f0946dd0
calvince <calmosh1421@gmail.com>
2023-07-06 22:14:06 +0300

    b2c async response

--- src/main/java/dev/vexla/mpesaDaraja/dto/response/B2CTransactionAsyncResponse.java
+++ src/main/java/dev/vexla/mpesaDaraja/dto/response/B2CTransactionAsyncResponse.java
@@ -1,10 +1,13 @@
 package dev.vexla.mpesaDaraja.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 @Data
 public class B2CTransactionAsyncResponse {
 
+    @JsonProperty("Result")
+    private Result result;
 
 }
 
