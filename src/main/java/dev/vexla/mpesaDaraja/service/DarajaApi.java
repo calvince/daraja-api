53af8c35e6fa2f8422c4a0c9a84e50f92de44c18
calvince <calmosh1421@gmail.com>
2023-07-10 18:53:27 +0300

    interface method to get transaction result

--- src/main/java/dev/vexla/mpesaDaraja/service/DarajaApi.java
+++ src/main/java/dev/vexla/mpesaDaraja/service/DarajaApi.java
@@ -1,11 +1,9 @@
 package dev.vexla.mpesaDaraja.service;
 
 import dev.vexla.mpesaDaraja.dto.request.InternalB2CTransactionRequest;
+import dev.vexla.mpesaDaraja.dto.request.InternalTransactionStatusRequest;
 import dev.vexla.mpesaDaraja.dto.request.SimulateC2BRequest;
-import dev.vexla.mpesaDaraja.dto.response.B2CTransactionSyncResponse;
-import dev.vexla.mpesaDaraja.dto.response.SimulateC2BResponse;
-import dev.vexla.mpesaDaraja.dto.response.AccessToken;
-import dev.vexla.mpesaDaraja.dto.response.RegisterUrlResponse;
+import dev.vexla.mpesaDaraja.dto.response.*;
 
 public interface DarajaApi {
     AccessToken getAccessToken();
@@ -14,4 +12,6 @@ public interface DarajaApi {
     SimulateC2BResponse simulateC2BTransaction(SimulateC2BRequest simulateC2BRequest);
 
     B2CTransactionSyncResponse performB2CTransaction(InternalB2CTransactionRequest b2CTransactionRequest);
+
+    TransactionStatusSyncResponse getTransactionResult(InternalTransactionStatusRequest request);
 }
