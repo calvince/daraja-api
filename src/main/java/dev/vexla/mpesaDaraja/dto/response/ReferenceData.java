6ac60a927ff45e4b70ee1ea37fd8e58d7c2d2043
calvince <calmosh1421@gmail.com>
2023-07-12 14:34:31 +0300

    ReferenceItem object

---
+++ src/main/java/dev/vexla/mpesaDaraja/dto/response/ReferenceData.java
@@ -0,0 +1,9 @@
+package dev.vexla.mpesaDaraja.dto.response;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class ReferenceData{
+
+	@JsonProperty("ReferenceItem")
+	private ReferenceItem referenceItem;
+}
