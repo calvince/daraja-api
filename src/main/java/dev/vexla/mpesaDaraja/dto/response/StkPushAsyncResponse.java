f517cef6c68879510368604a7519fb6e7c4099b4
calvince <calmosh1421@gmail.com>
2023-07-13 06:15:30 +0300

    mpesa express async response object

---
+++ src/main/java/dev/vexla/mpesaDaraja/dto/response/StkPushAsyncResponse.java
@@ -0,0 +1,11 @@
+package dev.vexla.mpesaDaraja.dto.response;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+
+@Data
+public class StkPushAsyncResponse{
+
+	@JsonProperty("Body")
+	private Body body;
+}
