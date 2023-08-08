134767bd40b44bc47350db603ca1b13690028b0b
calvince <calmosh1421@gmail.com>
2023-08-08 05:14:41 +0300

    initial commit

---
+++ src/main/java/dev/vexla/mpesaDaraja/entity/StkPush_Entries.java
@@ -0,0 +1,43 @@
+package dev.vexla.mpesaDaraja.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "stk_push_entries")
+public class StkPush_Entries {
+    @Id
+    @Column(name = "internal_id", nullable = false)
+    private String internalId;
+
+    @Column(name = "transaction_type")
+    private String transactionType;
+
+    @Column(name = "msisdn")
+    private String msisdn;
+
+    @Column(name = "amount")
+    private String amount;
+
+    @Column(name = "merchant_request_id", unique = true)
+    private String merchantRequestID;
+
+    @Column(name = "checkout_request_id", unique = true)
+    private String checkoutRequestID;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "entry_date")
+    private Date entryDate;
+
+    @Column(name = "result_code")
+    private String resultCode;
+
+    @Column(name = "raw_callback_payload_response", length = 500)
+    private String rawCallbackPayloadResponse;
+
+}
